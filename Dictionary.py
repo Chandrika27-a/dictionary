cd94d0d4afb36cdfaede71058e43d2ca76b9adff
Chandrika27-a <66053888+Chandrika27-a@users.noreply.github.com>
2020-05-28 15:14:13 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,4 @@
+my_dict = {"chandu" : 29,"yamini" : 27,"anji" : 35,"Lakshmi" : 25}
+print ("The dictionary before performing remove is:"+str(my_dict))
+del my_ dict['yamini']
+print ("The dictionary after remove is"+str(my_dict))
